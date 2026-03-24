dca856d6cea1a6c65cca316b8036c3a104caa902
2656172491 <2656172491@qq.com>
2026-03-24 20:36:39 +0800

    最初的项目文件

---
+++ src/main/java/org/example/yansispringboot/YansiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.yansispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YansiSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YansiSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/yansispringboot/YansiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yansispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YansiSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
