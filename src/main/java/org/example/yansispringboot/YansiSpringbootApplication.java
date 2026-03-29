2bb6cc91aeeca3eb081c46eb6720c6f8d5cbb79f
2656172491 <2656172491@qq.com>
2026-03-29 10:29:05 +0800

    解决跨域问题

--- src/main/java/org/example/yansispringboot/YansiSpringbootApplication.java
+++ src/main/java/org/example/yansispringboot/YansiSpringbootApplication.java
@@ -2,8 +2,10 @@ package org.example.yansispringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class YansiSpringbootApplication {
 
     public static void main(String[] args) {
